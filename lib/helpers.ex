b5c05871aed6c32d283a0523b9505a07c14400de
Todd Epple <todd@epples.com>
2019-05-23 08:10:43 -0400

    Turn off debugging

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -236,7 +236,7 @@ defmodule AddressUS.Parser.Helpers do
   end
 
   def log_term(term \\ nil, label) do
-    Logger.debug(label <> ": " <> inspect(term))
+    # Logger.debug(label <> ": " <> inspect(term))
     term
   end
 
